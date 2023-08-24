4245dc73caf9952d84a118ff56435e8892346497
amitgiri257 <127419759+amitgiri257@users.noreply.github.com>
2023-08-24 21:09:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,5 @@ function decimalToBinary(num) {
 	 return num.toString(2);
 }
  
-console.log(decimalToBinary(7));
-console.log(decimalToBinary(10)); 
-console.log(decimalToBinary(33));
-  
 
 window.decimalToBinary = decimalToBinary;
